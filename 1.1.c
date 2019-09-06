5c69d93e03256b5a11d9af298b2c2d7137a79775
mojo1979 <mojo@sheridancollege.ca>
2019-09-06 09:22:59 -0400

    Function Implementations on 1.1 and 1.2

--- 1.1.c
+++ 1.1.c
@@ -1,16 +1,19 @@
-/* Filename: 1.1.c 
+/* Filename: 1.1.c
  * Name(s): Daniel Nawrocki, John Mo
  * Created: September 6th, 2019 3:00 AM
  * Program Title: Assignment 1
- * Program Description: 
+ * Program Description: This program lists out Armstrong Numbers based on user
+ 												Parameters entered at execution.
  */
 
 // Headers and Gl. Variable Declarations
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
 
 // Function Prototypes
-
+bool isArmstrongNumber ( int power, int number );
 
 // Main
 int main ( int argc, char *argv[] ) {
@@ -20,3 +23,19 @@ int main ( int argc, char *argv[] ) {
 }
 
 // Functions
+bool isArmstrongNumber ( int power, int number ) {
+	// Local variables to hold sum of powered digits and digit divisor
+	int sum = 0, currentDivisor = 1;
+
+	// Sum each digit to the power
+	for ( int i = 0; i < power; i++ ) {
+		sum += pow((number / currentDivisor) % 10, power);
+		currentDivisor *= 10;
+	}
+
+	// if sum equals the number it is an Armstrong Number
+	if ( number == sum ) {
+		return true;
+	}
+	return false;
+}
--- 1.2.c
+++ 1.2.c
@@ -1,8 +1,9 @@
-/* Filename: 1.2.c 
+/* Filename: 1.2.c
  * Name(s): Daniel Nawrocki, John Mo
  * Created: September 6th, 2019 3:00 AM
  * Program Title: Assignment 1
- * Program Description: 
+ * Program Description: This program calculates euler's totient (phi) of a range
+ 											  of numbers.
  */
 
 // Headers and Gl. Variable Declarations
@@ -10,7 +11,7 @@
 #include<stdio.h>
 
 // Function Prototypes
-
+int gcd ( int x, int y );
 
 // Main
 int main ( int argc, char *argv[] ) {
@@ -20,3 +21,12 @@ int main ( int argc, char *argv[] ) {
 }
 
 // Functions
+int gcd ( int x, int y ) {
+	// End Euclidean Algorithm when remainder is zero
+	if ( y == 0 ) {
+		// Return GCD value
+		return x;
+	}
+	// Continue Euclidean Algorithm Recursively
+	return gcd( y, x % y );
+}
