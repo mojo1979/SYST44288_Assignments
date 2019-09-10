a602a52ca57512898e83adf7fcddfd1fd2965f5b
mojo1979 <mojo@sheridancollege.ca>
2019-09-10 00:11:51 -0400

    General bug fix, new function, and comment edits

--- 1.3.c
+++ 1.3.c
@@ -2,35 +2,46 @@
  * Name(s): Daniel Nawrocki, John Mo
  * Created: September 6th, 2019 3:00 AM
  * Program Title: Assignment 1
- * Program Description:
+ * Program Description: Implements an function (rmchr) to remove a character from a string.
  */
 
 // Headers and Gl. Variable Declarations
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 // Function Prototypes
 void rmchr ( char * x, char y );
 
 // Main
 int main ( int argc, char *argv[] ) {
+	// Call rmchr with argv[1] as string and argv[2][0] as char to remove
 	rmchr(argv[1],argv[2][0]);
+
+	// Print resulting string from argv[1]
 	printf("%s\n", argv[1]);
-	// Begin Coding Here!
 
 	return 0;
 }
 
 // Functions
 void rmchr ( char * x, char  y ){
-	for(int i = 0; i<sizeof(x); i++){
-		if (x[i] == y)
-		{
-			x[i] = x[i+1];
-			for (int j = i+1; j < sizeof(x)-1; j++) {
+	// For each charater of the string
+	for( int i = 0; i < strlen(x); i++) {
+
+		// If current character is equal to character to remove
+		if ( x[i] == y ) {
+
+			// Starting at index i, shift all letters down
+			for ( int j = i; j < strlen(x)-1; j++) {
 				x[j] = x[j+1];
 			}
+
+			// Set last char as string terminator char val 0
+			x[strlen(x)-1] = 0;
+
+			// Set index back to check if char is still equal to character to remove
+			i--;
 		}
 	}
-
 }
--- 1.4.c
+++ 1.4.c
@@ -1,22 +1,27 @@
-/* Filename: 1.4.c 
+/* Filename: 1.4.c
  * Name(s): Daniel Nawrocki, John Mo
  * Created: September 6th, 2019 3:00 AM
  * Program Title: Assignment 1
- * Program Description: 
+ * Program Description: Implements a function (rmstr) to remove a set of characters from a string
  */
 
 // Headers and Gl. Variable Declarations
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 // Function Prototypes
-
+void rmstr ( char * x, char * y );
 
 // Main
 int main ( int argc, char *argv[] ) {
 	// Begin Coding Here!
-
+	rmstr( argv[1], argv[2] );
+	printf("%s\n", argv[1]);
 	return 0;
 }
 
 // Functions
+void rmstr ( char * x, char * y ) {
+	
+}
