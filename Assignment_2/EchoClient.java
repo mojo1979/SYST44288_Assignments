8f42fc37157fb2339a0af052d05ef1c6edeab760
NAWROCKD <nawrockd@sheridancollege.ca>
2019-10-01 20:04:41 -0400

    added printwriter, added if statement to exit client, slightly formatted

--- Assignment_2/EchoClient.java
+++ Assignment_2/EchoClient.java
@@ -11,21 +11,24 @@ import java.util.*;
 public class EchoClient {
   // Global Variables
   public static void main(String[] args) {
-    try {
-      /* make connection to server socket */
-      Socket socket = new Socket("127.0.0.1",6013);
-      InputStream input = socket.getInputStream();
-      BufferedReader bin = new
-      BufferedReader(new InputStreamReader(input));
-      /* read the date from the socket */
-      String line;
-      while ( (line = bin.readLine()) != null)
-      System.out.println(line);
-     /* close the socket connection*/
-      socket.close();
-    }
-    catch (IOException ioe) {
-      System.err.println(ioe);
-    }
-  }
+    try (
+        Socket Sock = new Socket(args[0], 6013);
+        PrintWriter out = new PrintWriter(Sock.getOutputStream(), true);
+        InputStream in = Sock.getInputStream();
+        BufferedReader bin =new BufferedReader(new InputStreamReader(in));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+    ) {
+        String line;
+        while ((line = stdIn.readLine()) != null) {
+            out.println(line);
+            if (line.charAt(0) == '.'){
+              break;
+            }
+            System.out.println("Server: " + bin.readLine());
+            }
+        Sock.close();
+    }  catch (IOException ioe) {
+System.err.println(ioe);
+}
+}
 }
