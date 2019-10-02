fdd8868f54265cd84ec0b0217b82ec4e9d7def0f
NAWROCKD <nawrockd@sheridancollege.ca>
2019-10-01 20:03:23 -0400

    Added bufferReader, added while loop, slightly formatted

--- Assignment_2/EchoServer.java
+++ Assignment_2/EchoServer.java
@@ -1,3 +1,5 @@
+
+
 /* Filename: EchoServer.java
  * Name(s): Daniel Nawrocki, John Mo
  * Created: September 14th, 2019 8:30 PM
@@ -11,20 +13,22 @@ import java.util.*;
 public class EchoServer {
   // Global Variables
   public static void main(String[] args) {
-try{
-    ServerSocket socket = new ServerSocket(6013);
+    try {
+               ServerSocket sock =  new ServerSocket(6013);
+               Socket client = sock.accept();
+               PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+               InputStream inp = client.getInputStream();
+               BufferedReader in = new BufferedReader(new InputStreamReader(inp));
+               String input;
+               
+              while(true){
+                input = in.readLine();
+                out.println(input);
 
-    while (true) {
-        Socket clientserver = socket.accept();
-        PrintWriter printout = new PrintWriter(clientserver.getOutputStream(), true);
-        printout.println(new java.util.Date().toString());
-        clientserver.close();
-    }
-}
+             }
+           }    catch (IOException ioe) {
 
-
-  catch (IOException ioe) {
-System.err.println(ioe);
-  }
-}
-}
+                System.err.println(ioe);
+           }
+       }
+   }
